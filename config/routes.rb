8d9c0f3db8acb63fbdbc433c149327e2f0bde987
Jhon F. Martínez <jmartinez@bildu.co>
2022-04-07 00:56:44 -0500

    Estilos básicos y navbar. Inicio

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-
-
+  get 'static_pages/index'
+  get 'dashboard/index'
   devise_for :users
-  root to: "dashboard#index"
+  root to: 'static_pages#index'
 end
