6dafebc85ae259b547d341e6a71de2b97af03537
Jhon F. Martínez <jmartinez@bildu.co>
2022-04-09 03:18:11 -0500

    Flujo de seguimiento mejorado

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,14 +1,15 @@
 class PeopleController < ApplicationController
+  before_action :set_person, only: [:show]
   def index
     @people = Person.where.not(id: current_user.id)
   end
 
-  def show
-  end
+  def show; end
 
   private
 
   def set_person
+    redirect_to dashboard_index_path if params[:id].to_i == current_user.id
     @person = Person.find params[:id].to_i
   end
 end
